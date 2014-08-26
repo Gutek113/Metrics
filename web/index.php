0b60728601d07e386667a9dbd2faed269cfba34e
John New <john@hotmail.com>
2014-08-26 19:25:01 +0000

    Adding report

--- web/index.php
+++ web/index.php
@@ -3,8 +3,9 @@
 require '../vendor/autoload.php';
 
 use WP\Bundle\CaseBundle\Model\Person;
+use WP\Bundle\CaseBundle\Model\Adress;
 
-$adres = new WP\Bundle\CaseBundle\Model\Adress();
+$adres = new Adress();
 $person = new Person($adres, 'Janek');
-#echo $person->getName();
+echo $person->getName();
 
